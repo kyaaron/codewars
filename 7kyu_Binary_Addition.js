b7f8c929839d89cbfb99c53fb819a5d0a5e410cb
Aaron McCollum <59707347+kyaaron@users.noreply.github.com>
2026-02-02 15:46:51 +0900

    Create 7kyu_Binary_Addition.js

---
+++ 7kyu_Binary_Addition.js
@@ -0,0 +1,9 @@
+/**
+* Implement a function that adds two numbers together and returns their sum in binary. The conversion can be done before, or after the addition.
+* The binary number returned should be a string.
+*/
+
+const addBinary = (a,b) => {
+  const base10Sum = a + b;
+  return base10Sum.toString(2);
+}
