b97359c7c40ca75dcffc6830a429841644c8ae38
Aaron McCollum <59707347+kyaaron@users.noreply.github.com>
2026-02-27 13:41:06 +0900

    Implement highAndLow function for number extraction
    Added a function to find the highest and lowest numbers in a space-separated string.

---
+++ 7kyu_Highest_and_Lowest.js
@@ -0,0 +1,12 @@
+/**
+* Link: https://www.codewars.com/kata/554b4ac871d6813a03000035/train/javascript
+* In this little assignment you are given a string of space separated numbers, and have to return the highest and lowest number.
+* All numbers are valid Int32, no need to validate them.
+* There will always be at least one number in the input string.
+* Output string must be two numbers separated by a single space, and highest number is first.
+*/
+
+const highAndLow = (numbers) => {
+  const numArray = numbers.split(" ");
+  return `${Math.max(...numArray)} ${Math.min(...numArray)}`;
+}
